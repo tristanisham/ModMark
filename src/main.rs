f2b1f811ada1e02602ebbba61bdca0f466d056d9
Tristan Isham <tristan@isham.co>
2020-08-29 17:43:51 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ use modmark::Config;
 
 fn main() {
 
-    println!("Please enter your input file and output file");
-
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
